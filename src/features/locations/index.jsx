ea28de1a44ad117c57881342aae897d2e97921fa
Manny <parra.manny@gmail.com>
2022-06-14 11:22:36 -0600

    wrote css modules to show use of low-level responsive css

--- src/features/locations/index.jsx
+++ src/features/locations/index.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
-import Hero from "@/ui/hero";
+import { useState } from 'react'
+import Hero from '@/ui/hero'
+import Town from './town'
+import styles from './style.module.css'
 
-import Town from "./town";
+export default function Locations({list}) {
+  const [query, setQuery] = useState('')
+  const hero = list[0]
 
-export default function Locations({ list }) {
-  const [query, setQuery] = useState("");
+  const towns = list.slice(1, list.length)
 
-  const hero = list[0];
-  const towns = list.slice(1, list.length);
 
   return (
-    <>
-      <Hero {...hero} />
+      <>
 
-      <div className="flex items-center flex-wrap md:flex-nowrap gap-8 my-8">
-        {towns.map((town) => (
-          <Town key={town.location}{...town} />
-        ))}
-      </div>
-    </>
-  );
+        <Hero {...hero} />
+        <div className={styles["town-list-container"]}>
+          {towns.map((town) => (
+              <Town key={town.location}{...town} />
+          ))}
+        </div>
+      </>
+  )
 }
+
+
